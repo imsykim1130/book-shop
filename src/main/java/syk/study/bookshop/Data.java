71343dfe3d0ce974d1a1a8bf4bed13fa0fd820b4
imsykim1130 <imsykim1130@gmail.com>
2024-08-28 19:06:58 +0900

    chore : h2 내부 메모리, 초기 데이터 넣기

--- src/main/java/syk/study/bookshop/Data.java
+++ src/main/java/syk/study/bookshop/Data.java
@@ -4,18 +4,13 @@ import jakarta.annotation.PostConstruct;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Component;
 import org.springframework.transaction.annotation.Transactional;
-import syk.study.bookshop.entity.Book;
 import syk.study.bookshop.entity.User;
-import syk.study.bookshop.repository.BookRepository;
 import syk.study.bookshop.repository.UserRepository;
 
 @Component
 @RequiredArgsConstructor
 public class Data {
     private final UserDataInit userDataInit;
-
-    private String client_id;
-    private String client_secret;
 
     @PostConstruct
     public void init() {
